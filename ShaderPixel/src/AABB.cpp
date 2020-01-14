bb2e9315e50f83319ce9107c4e4b2426cc69b58a
skamoza <skamoza@gmail.com>
2020-01-14 19:42:51 +0200

    a little cleanup

--- ShaderPixel/src/AABB.cpp
+++ ShaderPixel/src/AABB.cpp
@@ -14,28 +14,4 @@ void AABB::extend(glm::vec3 position)
 {
 	Max = glm::max(position, Max);
 	Min = glm::min(position, Min);
-
-	return;
-
-
-	
-
-
-
-
-
-
-
-	if (position.x < Min.x)
-		Min.x = position.x;
-	if (position.y < Min.y)
-		Min.y = position.y;
-	if (position.z < Min.z)
-		Min.z = position.z;
-	if (position.x > Max.x)
-		Max.x = position.x;
-	if (position.y > Max.y)
-		Max.y = position.y;
-	if (position.z > Max.z)
-		Max.z = position.z;
 }
